1b0a58d82a64fd7073251b2de5d3f4d8943ed605
REWY432 <andrea.titov365@gmail.com>
2025-12-31 01:15:36 +0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App';
-import './index.css'; // Optional if you add custom CSS later
 
 const queryClient = new QueryClient({
   defaultOptions: {
