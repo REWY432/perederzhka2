4b9693866b0c44a62546a533b781933efd863d11
REWY432 <andrea.titov365@gmail.com>
2025-12-31 02:01:09 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Для GitHub Pages - замените 'perederzhka2' на имя вашего репозитория
+  base: '/perederzhka2/',
   resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './'),
-    },
     dedupe: ['react', 'react-dom']
   },
   build: {
